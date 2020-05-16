241ac5bc81550beeb085c83bd231f9b2b96a60e6
Mukhtar MAULIMOV <maulimo_m@qwasar.io>
2020-05-16 14:47:27 +0000

    adding my_str_len and my_write healpers

--- main.c
+++ main.c
@@ -1,5 +1,10 @@
 
 int main( int argc, const char* argv[] )
 {
+    if (argc < 3)
+    {
+        
+    }
+
     return 0;
 }
---
+++ tar_main.h
@@ -0,0 +1,5 @@
+
+#include <stddef.h>
+
+void my_write(int fd, char *input);
+size_t my_str_len(char *input);
