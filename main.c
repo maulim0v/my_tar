92bf184470d8d30b4138cfb1ec439dda5c9d67be
Mukhtar Maulimov <maulimov@torc.ai>
2020-05-17 17:02:43 -0400

    adding missing files

--- main.c
+++ main.c
@@ -1,10 +1,26 @@
 
+#include "my_tar_main.h"
+
+#include <sys/stat.h>
+
 int main( int argc, const char* argv[] )
 {
     if (argc < 3)
     {
-        
+        my_str_write(1, "Not enough input! Exiting...\n");
+        return 1;
     }
+
+    int num_files = my_int_max(argc - 3, 0);
+
+    const char *filename = argv[2];
+    const char *files[num_files];
+    for (size_t i = 3; i < argc; ++i)
+    {
+        files[i - 3] = argv[i];
+    }
+
+
 
     return 0;
 }
--- tar_main.h
+++
@@ -1,5 +0,0 @@
-
-#include <stddef.h>
-
-void my_write(int fd, char *input);
-size_t my_str_len(char *input);
